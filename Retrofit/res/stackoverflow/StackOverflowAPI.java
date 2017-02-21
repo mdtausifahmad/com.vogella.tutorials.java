27b8c7c6d0eecaf3c69af78f8bae64cc6374cfa8
Simon <simon.scholz@vogella.com>
2017-02-21 11:13:09 +0100

    [Retrofit] Removed unnecessary class for Retrofit api class
    Change-Id: Ifc6b918415bf0e40dc4475c6097fcaa298daa562
    Signed-off-by: Simon <simon.scholz@vogella.com>

--- Retrofit/res/stackoverflow/StackOverflowAPI.java
+++ Retrofit/res/stackoverflow/StackOverflowAPI.java
@@ -1,10 +1,12 @@
 package com.vogella.android.retrofitstackoverflow;
 
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface StackOverflowAPI {
     @GET("/2.2/questions?order=desc&sort=creation&site=stackoverflow")
-    Call<StackOverflowQuestions> loadQuestions(@Query("tagged") String tags);
+    Call<List<Question>> loadQuestions(@Query("tagged") String tags);
 }
